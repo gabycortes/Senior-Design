b964114d3bbac2b94981460f52cd19b03f94f8ba
edvos <47930552+edvos@users.noreply.github.com>
2019-09-25 13:00:16 -0700

    Delete simpletest.py

--- simpletest.py
+++
@@ -1,5 +0,0 @@
-import Adafruit_GPIO as SPI
-import adafruit_mcp3xxx.mcp3008 as MCP
-from adafruit_mcp3xxx.analog_in import AnalogIn
-
-spi = busio.SPI(clock = board.SCK, MISO = board.MISO, MOSI = board.MOSI)
