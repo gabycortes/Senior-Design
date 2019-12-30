040eb55825db81dd221deeaf59ad07ad086c431f
Krystal Bernal <19231900+krystal-bc@users.noreply.github.com>
2019-12-30 12:28:37 -0800

    change to window order

--- GUIpractice.py
+++ GUIpractice.py
@@ -1,69 +1,84 @@
 from tkinter import *
+from datetime import date
 
-def start():
-    global lbl_status
-    lbl_status.config(text = "Starting test run")
-    createNew = Tk()
-    createNew.title("New Test Run")
+def home():
 
-    lbl_enter_test_type = Label(createNew, text = "Type of test run: ")
-    lbl_enter_primary_weight = Label(createNew, text = "CVT primary weight: ")
-    lbl_enter_primary_spring = Label(createNew, text = "CVT primary spring: ")
-    lbl_enter_secondary_spring = Label(createNew, text = "CVT secondary spring: ")
-    lbl_enter_secondary_clock = Label(createNew, text = "CVT secondary clock: ")
+    today = date.today()
+    today = today.strftime("%m-%d-%y")
 
-    entry_test_type = Entry(createNew)
-    entry_primary_weight = Entry(createNew)
-    entry_primary_spring = Entry(createNew)
-    entry_secondary_spring = Entry(createNew)
-    entry_secondary_clock = Entry(createNew)
-    # entry.get() saves the text
-    
-    lbl_enter_test_type.grid(row = 0, column = 0)
-    entry_test_type.grid(row = 0, column = 1)
-    
-    lbl_enter_primary_weight.grid(row = 1, column = 0)
-    entry_primary_weight.grid(row = 1, column = 1)
+    global file
+    global entry_test_type
+    global entry_primary_weight
+    global entry_primary_spring
+    global entry_secondary_spring
+    global entry_secondary_clock
 
-    lbl_enter_secondary_spring.grid(row = 2, column = 0)
-    entry_secondary_spring.grid(row = 2, column = 1)
+    file = open("HEDATA_"+today+".csv", "w+")
+    file.write(str(entry_test_type.get())+'\n')
+    file.write(str(entry_primary_weight.get())+'\n')
+    file.write(str(entry_primary_spring.get())+'\n')
+    file.write(str(entry_secondary_spring.get())+'\n')
+    file.write(str(entry_secondary_clock.get())+'\n')
+    file.close()
 
-    lbl_enter_secondary_clock.grid(row = 3, column = 0)
-    entry_secondary_clock.grid(row = 3, column = 1)
+    # initial GUI components -------------------------
+    root.title("Cal State LA Baja SAE CVT Test")
+    root.geometry("500x500+100+100")
 
-    # create method to save entries before quitting
-    btn_ok = Button(createNew, text="OK", padx = 20, command=createNew.destroy)
-    btn_ok.grid(row = 4, column = 0)
-    
+    lbl_status = Label(root, text = "New test run created, ready to begin.")
+    lbl_status.grid(row = 1, column = 0)
 
-def stop():
-    # needs troubleshooting
-    global root
-    global lbl_status
+    lbl_primarytitle = Label(root, text = "Primary RPM")
+    lbl_primarytitle.grid(row = 2, column = 0)
+
+    lbl_secondarytitle = Label(root, text = "Secondary RPM")
+    lbl_secondarytitle.grid(row = 2, column = 1)
+
+    #to pass parameters to command use
+    # command = lambda: method_name(args)
+    btn_start = Button(root, text="Start Test Run", padx = 20, command =lambda: start(lbl_status))
+    btn_start.grid(row = 0, column = 0)
+
+    btn_stop = Button(root, text="Stop Test Run", padx = 20, command =lambda: stop(lbl_status))
+    btn_stop.grid(row = 0, column = 1)
+
+def start(lbl):
+    lbl.set(text = "Started test run")
+
+def stop(lbl_status):
     lbl_status.config(text = "Stopped test run")
 
-
-
-# initial GUI components -------------------------
 root = Tk()
-root.title("Cal State LA Baja SAE CVT Test")
-root.geometry("500x500+100+100")
+root.title("New Test Run")
 
-#to pass parameters to command use
-# command = lambda: method_name(args)
-btn_start = Button(root, text="Start Test Run", padx = 20, command=start)
-btn_start.grid(row = 0, column = 0)
+lbl_enter_test_type = Label(root, text = "Type of test run: ")
+lbl_enter_primary_weight = Label(root, text = "CVT primary weight: ")
+lbl_enter_primary_spring = Label(root, text = "CVT primary spring: ")
+lbl_enter_secondary_spring = Label(root, text = "CVT secondary spring: ")
+lbl_enter_secondary_clock = Label(root, text = "CVT secondary clock: ")
 
-btn_stop = Button(root, text="Stop Test Run", padx = 20, command = stop)
-btn_stop.grid(row = 0, column = 1)
+entry_test_type = Entry(root)
+entry_primary_weight = Entry(root)
+entry_primary_spring = Entry(root)
+entry_secondary_spring = Entry(root)
+entry_secondary_clock = Entry(root)
 
-lbl_status = Label(root, text = "")
-lbl_status.grid(row = 1, column = 0)
+lbl_enter_test_type.grid(row = 0, column = 0)
+entry_test_type.grid(row = 0, column = 1)
 
-lbl_primarytitle = Label(root, text = "Primary RPM")
-lbl_primarytitle.grid(row = 2, column = 0)
+lbl_enter_primary_weight.grid(row = 1, column = 0)
+entry_primary_weight.grid(row = 1, column = 1)
 
-lbl_secondarytitle = Label(root, text = "Secondary RPM")
-lbl_secondarytitle.grid(row = 2, column = 1)
+lbl_enter_primary_spring.grid(row = 2, column = 0)
+entry_primary_spring.grid(row = 2, column = 1)
 
-root.mainloop()
+lbl_enter_secondary_spring.grid(row = 3, column = 0)
+entry_secondary_spring.grid(row = 3, column = 1)
+
+lbl_enter_secondary_clock.grid(row = 4, column = 0)
+entry_secondary_clock.grid(row = 4, column = 1)
+
+# create method to save entries before quitting
+btn_ok = Button(root, text="OK", padx = 20, command=home)
+btn_ok.grid(row = 5, column = 0)
+
