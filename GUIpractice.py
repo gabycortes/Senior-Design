3113f6b9d33445906ebfa1ef7244a5e99a6e4f43
Krystal Bernal <19231900+krystal-bc@users.noreply.github.com>
2020-01-03 12:26:43 -0800

    implement OO approach
    button functionality still required

--- GUIpractice.py
+++ GUIpractice.py
@@ -1,84 +1,119 @@
-from tkinter import *
-from datetime import date
+import tkinter as tk                # python 3
+from tkinter import font  as tkfont # python 3
+#import Tkinter as tk     # python 2
+#import tkFont as tkfont  # python 2
 
-def home():
+class SampleApp(tk.Tk):
 
-    today = date.today()
-    today = today.strftime("%m-%d-%y")
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
 
-    global file
-    global entry_test_type
-    global entry_primary_weight
-    global entry_primary_spring
-    global entry_secondary_spring
-    global entry_secondary_clock
+        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
 
-    file = open("HEDATA_"+today+".csv", "w+")
-    file.write(str(entry_test_type.get())+'\n')
-    file.write(str(entry_primary_weight.get())+'\n')
-    file.write(str(entry_primary_spring.get())+'\n')
-    file.write(str(entry_secondary_spring.get())+'\n')
-    file.write(str(entry_secondary_clock.get())+'\n')
-    file.close()
+        # the container is where we'll stack a bunch of frames
+        # on top of each other, then the one we want visible
+        # will be raised above the others
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-    # initial GUI components -------------------------
-    root.title("Cal State LA Baja SAE CVT Test")
-    root.geometry("500x500+100+100")
+        self.frames = {}
+        for F in (StartPage, InputPage):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
 
-    lbl_status = Label(root, text = "New test run created, ready to begin.")
-    lbl_status.grid(row = 1, column = 0)
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0, sticky="nsew")
 
-    lbl_primarytitle = Label(root, text = "Primary RPM")
-    lbl_primarytitle.grid(row = 2, column = 0)
+        self.show_frame("StartPage")
 
-    lbl_secondarytitle = Label(root, text = "Secondary RPM")
-    lbl_secondarytitle.grid(row = 2, column = 1)
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.tkraise()
 
-    #to pass parameters to command use
-    # command = lambda: method_name(args)
-    btn_start = Button(root, text="Start Test Run", padx = 20, command =lambda: start(lbl_status))
-    btn_start.grid(row = 0, column = 0)
 
-    btn_stop = Button(root, text="Stop Test Run", padx = 20, command =lambda: stop(lbl_status))
-    btn_stop.grid(row = 0, column = 1)
+class StartPage(tk.Frame):
 
-def start(lbl):
-    lbl.set(text = "Started test run")
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
 
-def stop(lbl_status):
-    lbl_status.config(text = "Stopped test run")
+        #self.title("Cal State LA Baja SAE CVT Test")
+        #self.geometry("500x500+100+100")
 
-root = Tk()
-root.title("New Test Run")
+        lbl_status = tk.Label(self, text = "New test run created, ready to begin.")
+        lbl_status.grid(row = 1, column = 0)
 
-lbl_enter_test_type = Label(root, text = "Type of test run: ")
-lbl_enter_primary_weight = Label(root, text = "CVT primary weight: ")
-lbl_enter_primary_spring = Label(root, text = "CVT primary spring: ")
-lbl_enter_secondary_spring = Label(root, text = "CVT secondary spring: ")
-lbl_enter_secondary_clock = Label(root, text = "CVT secondary clock: ")
+        lbl_primarytitle = tk.Label(self, text = "Primary RPM")
+        lbl_primarytitle.grid(row = 2, column = 0)
 
-entry_test_type = Entry(root)
-entry_primary_weight = Entry(root)
-entry_primary_spring = Entry(root)
-entry_secondary_spring = Entry(root)
-entry_secondary_clock = Entry(root)
+        lbl_secondarytitle = tk.Label(self, text = "Secondary RPM")
+        lbl_secondarytitle.grid(row = 2, column = 1)
 
-lbl_enter_test_type.grid(row = 0, column = 0)
-entry_test_type.grid(row = 0, column = 1)
+        #to pass parameters to command use
+        # command = lambda: method_name(args)
+        btn_start = tk.Button(self, text="Start Test Run", padx = 20, command =lambda: controller.show_frame("InputPage"))
+        btn_start.grid(row = 0, column = 0)
 
-lbl_enter_primary_weight.grid(row = 1, column = 0)
-entry_primary_weight.grid(row = 1, column = 1)
+        btn_stop = tk.Button(self, text="Stop Test Run", padx = 20)
+        btn_stop.grid(row = 0, column = 1)
 
-lbl_enter_primary_spring.grid(row = 2, column = 0)
-entry_primary_spring.grid(row = 2, column = 1)
 
-lbl_enter_secondary_spring.grid(row = 3, column = 0)
-entry_secondary_spring.grid(row = 3, column = 1)
+class InputPage(tk.Frame):
 
-lbl_enter_secondary_clock.grid(row = 4, column = 0)
-entry_secondary_clock.grid(row = 4, column = 1)
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        lbl_enter_test_type = tk.Label(self, text = "Type of test run: ")
+        lbl_enter_primary_weight = tk.Label(self, text = "CVT primary weight: ")
+        lbl_enter_primary_spring = tk.Label(self, text = "CVT primary spring: ")
+        lbl_enter_secondary_spring = tk.Label(self, text = "CVT secondary spring: ")
+        lbl_enter_secondary_clock = tk.Label(self, text = "CVT secondary clock: ")
 
-# create method to save entries before quitting
-btn_ok = Button(root, text="OK", padx = 20, command=home)
-btn_ok.grid(row = 5, column = 0)
+        entry_test_type = tk.Entry(self)
+        entry_primary_weight = tk.Entry(self)
+        entry_primary_spring = tk.Entry(self)
+        entry_secondary_spring = tk.Entry(self)
+        entry_secondary_clock = tk.Entry(self)
 
+        lbl_enter_test_type.grid(row = 0, column = 0)
+        entry_test_type.grid(row = 0, column = 1)
+
+        lbl_enter_primary_weight.grid(row = 1, column = 0)
+        entry_primary_weight.grid(row = 1, column = 1)
+
+        lbl_enter_primary_spring.grid(row = 2, column = 0)
+        entry_primary_spring.grid(row = 2, column = 1)
+
+        lbl_enter_secondary_spring.grid(row = 3, column = 0)
+        entry_secondary_spring.grid(row = 3, column = 1)
+
+        lbl_enter_secondary_clock.grid(row = 4, column = 0)
+        entry_secondary_clock.grid(row = 4, column = 1)
+
+        # create method to save entries before quitting
+        btn_ok = tk.Button(self, text="OK", padx = 20)
+        btn_ok.grid(row = 5, column = 0)
+
+
+
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="This is page 2", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Go to the start page",
+                           command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+
+
+if __name__ == "__main__":
+    app = SampleApp()
+    app.mainloop()
