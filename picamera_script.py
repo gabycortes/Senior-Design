ce7037633e607e68c8d6657ee16cc2c718b985fb
Krystal Bernal <19231900+krystal-bc@users.noreply.github.com>
2020-02-05 15:13:53 -0800

    Add files via upload
    2/5/20
    Must be used with GUI

---
+++ picamera_script.py
@@ -0,0 +1,24 @@
+#bowen - fix shit
+
+import time
+import os
+import sys
+from picamera import PiCamera
+from datetime import datetime
+
+file_name = sys.argv[1]
+os.mkdir(file_name)
+
+camera = PiCamera()
+print(file_name)
+print("Started taking pictures")
+
+while True:
+	time_taken = time.asctime(time.gmtime())
+	ini_time_for_now = datetime.now()
+	current_time = ini_time_for_now.strftime("%H-%M-%S")
+	#camera.capture('%s/picamera_pictures/testpic%s.jpg' % file_name, current_time)
+	img_name = file_name + "/testpic" + current_time + ".jpg"
+	camera.capture(img_name)
+	camera.stop_preview()
+	time.sleep(3)
