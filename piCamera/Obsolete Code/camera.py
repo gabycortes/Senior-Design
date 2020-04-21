5ed291a60fac27e65e3ad90c79714f8729a3ac4a
edvos <47930552+edvos@users.noreply.github.com>
2020-04-21 15:38:45 -0700

    Upload old code

---
+++ piCamera/Obsolete Code/camera.py
@@ -0,0 +1,10 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+
+camera.start_preview(alpha=180)
+for i in range(5):
+    sleep(2)
+    camera.capture('/home/pi/Desktop/testpic%s.jpg' % i)
+camera.stop_preview()
