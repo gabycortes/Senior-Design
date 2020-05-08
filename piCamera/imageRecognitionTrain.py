1aa88cc0017a0d91087a694d49a3961fef4c4ffb
BowenWaugh <54382150+BowenWaugh@users.noreply.github.com>
2020-05-07 17:25:27 -0700

    Update imageRecognitionTrain.py

--- piCamera/imageRecognitionTrain.py
+++ piCamera/imageRecognitionTrain.py
@@ -1,8 +1,10 @@
 from imageai.Detection.Custom import DetectionModelTrainer
 
+# Initialize the model trainer and set it to YOLO (YOLOv3 is optional)
 train = DetectionModelTrainer()
 train.setModelTypeAsYOLOv3()
-# put your director name
+
+# Choose the name of your directory
 d_name = "tires"
 train.setDataDirectory(data_directory=d_name)
 train.setTrainConfig(object_names_array=[d_name], batch_size=4, num_experiments=203)
