6b61d2751c4f2dec2d49a0558085b88db331d66d
BowenWaugh <54382150+BowenWaugh@users.noreply.github.com>
2020-05-07 17:46:49 -0700

    Update picamera_script.py

--- piCamera/picamera_script.py
+++ piCamera/picamera_script.py
@@ -4,19 +4,26 @@ import sys
 from picamera import PiCamera
 from datetime import datetime
 
+# Create a folder to organize images into.
 file_name = sys.argv[1]
 os.mkdir(file_name)
 
+# Initialize the camera and notify when the script is active.
 camera = PiCamera()
 print(file_name)
 print("Started taking pictures")
 
 while True:
+	# Get the time the photo is taken.
 	time_taken = time.asctime(time.gmtime())
 	ini_time_for_now = datetime.now()
 	current_time = ini_time_for_now.strftime("%H-%M-%S")
-	#camera.capture('%s/picamera_pictures/testpic%s.jpg' % file_name, current_time)
+	
+	# Capture the photo and name it the current time.
+	camera.capture('%s/picamera_pictures/testpic%s.jpg' % file_name, current_time)
 	img_name = file_name + "/testpic" + current_time + ".jpg"
 	camera.capture(img_name)
 	camera.stop_preview()
+	
+	# Pause for three second.
 	time.sleep(3)
